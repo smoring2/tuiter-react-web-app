5d3a868e37b22c2b6b08c219d4d1144ee977e007
smoring2 <smoring1995@gmail.com>
2022-10-13 13:24:36 -0700

    a6 lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Labs from './labs';
+import HelloWorld from "./labs/a6/hello-world";
+import Tuiter from "./tuiter";
+import {BrowserRouter} from "react-router-dom";
+import {Route, Routes} from "react-router";
+import {Link} from "react-router-dom";
+import Nav from "./nav";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="container">
+                {/*<HelloWorld/>*/}
+                {/*<Labs/>*/}
+                {/*<Tuiter/>*/}
+                {/*<Nav/>*/}
+                <Routes>
+                    <Route index element={<Labs/>}/>
+                    <Route path="/hello" element={<HelloWorld/>}/>
+                    <Route path="/tuiter" element={<Tuiter/>}/>
+                </Routes>
+                {/*<h1>Footer</h1>*/}
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
