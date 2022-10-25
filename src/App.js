13c2aa08c8c51c2e91823ae5bcf19062dce38ce4
smoring2 <smoring1995@gmail.com>
2022-10-25 09:32:47 -0700

    a7 lab

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
                 {/*<Tuiter/>*/}
                 {/*<Nav/>*/}
                 <Routes>
-                    <Route index element={<Labs/>}/>
+                    <Route path="/*" element={<Labs/>}/>
                     <Route path="/hello" element={<HelloWorld/>}/>
                     <Route path="/tuiter/*" element={<Tuiter/>}/>
                 </Routes>
